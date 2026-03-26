59b300066f4c61cdf52345d8690d2b164c0f647b
rachad <rachdos76@gmail.com>
2026-03-26 11:58:14 +0100

    enregistre les interfaces necessaire dans AppServiceProvider pour les interfaces et repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\EntrepriseRepositoryInterface;
+use App\Interfaces\UserRepositoryInterface;
+use App\Repositories\EntrepriseRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(EntrepriseRepositoryInterface::class, EntrepriseRepository::class);
     }
 
     /**
